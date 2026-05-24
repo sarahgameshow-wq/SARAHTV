78845f942098b63e22a88c53274d1a2b446a4160
sarahgameshow-wq <sarahgameshow@gmail.com>
2026-05-24 15:12:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const firebaseConfig = { databaseURL: "https://sarahtv-19938-default-rtdb.firebaseio.com/" };
+const firebaseConfig = { databaseURL: "SUA_URL_AQUI" };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
@@ -7,9 +7,17 @@ db.ref('comando').on('value', (snapshot) => {
     const tela = document.getElementById('root');
     
     if (cmd && cmd.startsWith('PLAY:')) {
-        const idVideo = cmd.split(':')[1];
-        tela.innerHTML = `<iframe width="100%" height="400" src="https://www.youtube.com/embed/${idVideo}?autoplay=1" frameborder="0"></iframe>`;
+        let url = cmd.split('PLAY:')[1];
+        // Extrai o ID do vídeo (funciona com links normais e curtos)
+        let videoId = url.split('v=')[1] || url.split('/').pop();
+        if (videoId.indexOf('&') !== -1) videoId = videoId.split('&')[0];
+        
+        tela.innerHTML = `<iframe width="100%" height="400" src="https://www.youtube.com/embed/${videoId}?autoplay=1" frameborder="0" allow="autoplay; fullscreen"></iframe>`;
     } else {
         tela.innerHTML = "<h1>SISTEMA ONLINE</h1><p>Aguardando operação...</p>";
     }
 });
+
+setInterval(() => {
+    document.getElementById('relogio').innerText = new Date().toLocaleTimeString();
+}, 1000);
