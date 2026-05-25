b9f01a5bb98a62a27d01c47f27c5a6885e1bf42b
sarahgameshow-wq <sarahgameshow@gmail.com>
2026-05-25 09:00:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ const db = firebase.database();
 const monitorID = "Monitor_" + Math.floor(Math.random() * 1000);
 document.getElementById('id-monitor').innerText = monitorID;
 
-db.ref('monitores/' + monitorID).set({ status: 'online' });
+let playlist = [];
+let index = 0;
 
-db.ref('monitores/' + monitorID + '/comando').on('value', (snap) => {
-    const cmd = snap.val();
-    if (cmd && cmd.startsWith('PLAY:')) {
-        const url = cmd.replace('PLAY:', '');
-        const root = document.getElementById('root');
-        // Extrai o ID do YouTube de qualquer formato de link
-        const videoId = url.split('v=')[1]?.split('&')[0] || url.split('/').pop();
-        root.innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&modestbranding=1" style="width:100%;height:100%;border:none;"></iframe>`;
-    }
+// Ouve a lista de vídeos
+db.ref('monitores/' + monitorID + '/playlist').on('value', (snap) => {
+    playlist = [];
+    snap.forEach(child => playlist.push(child.val()));
+    if (playlist.length > 0 && index === 0) tocarProximo();
 });
+
+function tocarProximo() {
+    if (index >= playlist.length) index = 0; // Reinicia a playlist
+    const url = playlist[index];
+    const videoId = url.split('v=')[1]?.split('&')[0] || url.split('/').pop();
+    
+    document.getElementById('root').innerHTML = `
+        <iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&enablejsapi=1" 
+        style="width:100%;height:100%;border:none;" 
+        onload="setTimeout(tocarProximo, 60000)"></iframe>`; // O 60000 é um exemplo (em milissegundos)
+    
+    index++;
+}
