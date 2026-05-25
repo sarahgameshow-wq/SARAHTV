e2608d2daf67c89847244d76c3ef7e42da57cdc1
sarahgameshow-wq <sarahgameshow@gmail.com>
2026-05-25 09:13:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,25 +2,31 @@ const db = firebase.database();
 const monitorID = "Monitor_" + Math.floor(Math.random() * 1000);
 document.getElementById('id-monitor').innerText = monitorID;
 
-let playlist = [];
-let index = 0;
+db.ref('monitores/' + monitorID).set({ status: 'online' });
 
-// Ouve a lista de vídeos
+// Ouve a lista de vídeos (Playlist)
 db.ref('monitores/' + monitorID + '/playlist').on('value', (snap) => {
-    playlist = [];
+    const playlist = [];
     snap.forEach(child => playlist.push(child.val()));
-    if (playlist.length > 0 && index === 0) tocarProximo();
+    
+    if (playlist.length > 0) {
+        tocarPlaylist(playlist);
+    }
 });
 
-function tocarProximo() {
-    if (index >= playlist.length) index = 0; // Reinicia a playlist
-    const url = playlist[index];
-    const videoId = url.split('v=')[1]?.split('&')[0] || url.split('/').pop();
-    
-    document.getElementById('root').innerHTML = `
-        <iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&enablejsapi=1" 
-        style="width:100%;height:100%;border:none;" 
-        onload="setTimeout(tocarProximo, 60000)"></iframe>`; // O 60000 é um exemplo (em milissegundos)
-    
-    index++;
+function tocarPlaylist(lista) {
+    let index = 0;
+    function proximo() {
+        if (index >= lista.length) index = 0;
+        const url = lista[index];
+        const videoId = url.split('v=')[1]?.split('&')[0] || url.split('/').pop();
+        
+        document.getElementById('root').innerHTML = `
+            <iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&modestbranding=1" 
+            style="width:100%;height:100%;border:none;" 
+            onended="setTimeout(proximo, 1000)"></iframe>`;
+        
+        index++;
+    }
+    proximo();
 }
