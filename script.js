348e5571b1c8a05dcd378e99a2ec4e29b5741895
sarahgameshow-wq <sarahgameshow@gmail.com>
2026-05-25 08:53:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,15 +9,8 @@ db.ref('monitores/' + monitorID + '/comando').on('value', (snap) => {
     if (cmd && cmd.startsWith('PLAY:')) {
         const url = cmd.replace('PLAY:', '');
         const root = document.getElementById('root');
-        
-        // Lógica de Detecção
-        if (url.includes('youtube.com') || url.includes('youtu.be')) {
-            const id = url.split('v=')[1]?.split('&')[0] || url.split('/').pop();
-            root.innerHTML = `<iframe src="https://www.youtube.com/embed/${id}?autoplay=1&controls=0" style="width:100%;height:100%;border:none;"></iframe>`;
-        } else if (url.toLowerCase().endsWith('.mp4')) {
-            root.innerHTML = `<video src="${url}" autoplay loop muted style="width:100%;height:100%;"></video>`;
-        } else {
-            root.innerHTML = `<video src="${url}" autoplay loop muted style="width:100%;height:100%;"></video>`;
-        }
+        // Extrai o ID do YouTube de qualquer formato de link
+        const videoId = url.split('v=')[1]?.split('&')[0] || url.split('/').pop();
+        root.innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&modestbranding=1" style="width:100%;height:100%;border:none;"></iframe>`;
     }
 });
