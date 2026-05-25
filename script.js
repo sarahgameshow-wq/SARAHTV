2a734561da9988d73ea1d81396df281d0bc95eb5
sarahgameshow-wq <sarahgameshow@gmail.com>
2026-05-25 10:14:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,10 @@ const db = firebase.database();
 const monitorID = "Monitor_" + Math.floor(Math.random() * 1000);
 document.getElementById('id-monitor').innerText = monitorID;
 
+// Registra a tela no banco
 db.ref('monitores/' + monitorID).set({ status: 'online' });
 
-// Comando de Sistema (Reiniciar ou Girar)
-db.ref('monitores/' + monitorID + '/comando').on('value', (snap) => {
-    const cmd = snap.val();
-    if (cmd === 'REINICIAR') window.location.reload(true);
-    if (cmd === 'GIRAR') {
-        document.body.style.transform = "rotate(0deg)";
-        document.body.style.width = "100vw";
-        document.body.style.height = "100vh";
-    }
-});
-
-// Playlist
+// Loop de Playlist
 db.ref('monitores/' + monitorID + '/playlist').on('value', (snap) => {
     const playlist = [];
     snap.forEach(child => playlist.push(child.val()));
@@ -25,10 +15,25 @@ db.ref('monitores/' + monitorID + '/playlist').on('value', (snap) => {
 function tocarPlaylist(lista) {
     let index = 0;
     function proximo() {
-        if (index >= lista.length) index = 0;
+        if (index >= lista.length) index = 0; // Loop infinito
         const videoId = lista[index].split('v=')[1]?.split('&')[0] || lista[index].split('/').pop();
-        document.getElementById('root').innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&modestbranding=1" style="width:100%;height:100%;border:none;"></iframe>`;
+        
+        document.getElementById('root').innerHTML = `
+            <iframe src="https://www.youtube.com/embed/${videoId}?autoplay=1&controls=0&modestbranding=1" 
+            style="width:100%;height:100%;border:none;"
+            onended="setTimeout(proximo, 1000)"></iframe>`;
+        
+        // Pula para o próximo após o tempo do vídeo (simplificado)
+        setTimeout(proximo, 60000); // Ajuste este tempo ou use a API do YouTube
         index++;
     }
     proximo();
 }
+
+// Comandos de Manutenção
+db.ref('monitores/' + monitorID + '/comando').on('value', (snap) => {
+    if (snap.val() === 'REINICIAR') window.location.reload(true);
+    if (snap.val() === 'GIRAR') {
+        document.body.style.transform = "rotate(0deg)";
+    }
+});
